2101b18afaa3d6c0a24ed2f8859dd0d17043eca2
YifeiWang0618 <3441606266@qq.com>
2026-09-02 10:52:16 +0800

    feat: add admin charging station list

--- qt-server/services/adminmanagementservice.h
+++ qt-server/services/adminmanagementservice.h
@@ -10,6 +10,7 @@ public:
     explicit AdminManagementService(QSqlDatabase database);
     ResponseMessage pileList(const RequestMessage &request) const;
     ResponseMessage restartPile(const RequestMessage &request, qint64 adminId) const;
+    ResponseMessage stationList(const RequestMessage &request) const;
 
 private:
     QSqlDatabase m_database;
