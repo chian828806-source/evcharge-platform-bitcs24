4e0dff30f7d0e21aae989cf28908a2096dbf6466
YifeiWang0618 <3441606266@qq.com>
2026-09-02 18:53:42 +0800

    refactor: split admin pages and add refresh controls

--- qt-admin/mainwindow.h
+++ qt-admin/mainwindow.h
@@ -1,46 +1,30 @@
 #pragma once
-
-#include <QMainWindow>
+#include <QHash>
 #include <QJsonObject>
-
-class AdminSocketClient;
-class QLabel;
-class QLineEdit;
-class QPushButton;
-
+#include <QMainWindow>
+class AdminSocketClient; class DashboardPage; class QLabel; class QLineEdit;
+class PilePage; class QPushButton; class QTabWidget; class QTimer;
+class StationPage; class UserPage;
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
-public:
-    explicit MainWindow(QWidget *parent = nullptr);
-
+public: explicit MainWindow(QWidget *parent = nullptr);
 private:
     void submitLogin();
     void handleResponse(const QJsonObject &response);
-    void showRevenueSummary(const QJsonObject &data);
-    void showRevenueTrend(const QJsonObject &data);
-    void showPileStatusSummary(const QJsonObject &data);
-    void showPileList(const QJsonObject &data);
-    void showStationList(const QJsonObject &data);
-    void showUserList(const QJsonObject &data);
-
+    void buildManagementPages();
+    void refreshDashboard();
+    void requestPileList(const QJsonObject &payload = {});
+    void requestStationList();
+    void requestUserList();
+    QString send(const QString &type, const QJsonObject &payload = {});
+    void handleFailure(const QJsonObject &response);
     AdminSocketClient *m_client = nullptr;
-    QLineEdit *m_host = nullptr;
-    QLineEdit *m_port = nullptr;
-    QLineEdit *m_username = nullptr;
-    QLineEdit *m_password = nullptr;
-    QPushButton *m_login = nullptr;
-    QLabel *m_status = nullptr;
-    QString m_loginRequestId;
-    QString m_summaryRequestId;
-    QString m_trendRequestId;
-    QString m_pileStatusRequestId;
-    QString m_pileListRequestId;
-    QString m_pileRestartRequestId;
-    QString m_stationListRequestId;
-    QString m_stationCreateRequestId;
-    QString m_userListRequestId;
-    QString m_userStatusRequestId;
-    QString m_userPhoneKeyword;
-    QString m_sessionId;
+    QLineEdit *m_host = nullptr; QLineEdit *m_port = nullptr;
+    QLineEdit *m_username = nullptr; QLineEdit *m_password = nullptr;
+    QPushButton *m_login = nullptr; QLabel *m_status = nullptr;
+    QTabWidget *m_tabs = nullptr; DashboardPage *m_dashboard = nullptr;
+    PilePage *m_piles = nullptr; StationPage *m_stations = nullptr;
+    UserPage *m_users = nullptr; QTimer *m_dashboardTimer = nullptr;
+    QString m_sessionId; QHash<QString, QString> m_requestTypes;
 };
