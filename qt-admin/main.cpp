7be056a51fdb4d127e0a7ea1054e33a7b85a3879
FightingStella <3307590862@qq.com>
2026-09-03 20:30:08 +0800

    feat(admin-ui): implement management UI v1 with mock data

--- qt-admin/main.cpp
+++ qt-admin/main.cpp
@@ -1,11 +1,16 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QFile>
 
 int main(int argc, char *argv[])
 {
     QApplication application(argc, argv);
     QApplication::setApplicationName(QStringLiteral("EVCharge 管理端"));
+    QFile styleFile(QStringLiteral(":/admin/admin.qss"));
+    if (styleFile.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        application.setStyleSheet(QString::fromUtf8(styleFile.readAll()));
+    }
     MainWindow window;
     window.show();
     return application.exec();
